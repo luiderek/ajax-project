a5ce9167bffcf8f74dd92cac375570bf6156f0e0
Derek Lui <luiderek@outlook.com>
2022-04-18 19:09:01 +0000

    get JSON reply from  API call

--- js/main.js
+++ js/main.js
@@ -21,6 +21,20 @@ const $sidebarSearchbar = document.querySelector('form#search');
 
 $sidebarSearchbar.addEventListener('submit', function (event) {
   event.preventDefault();
-  // value of interest: $sidebarSearchbar.elements[0].value;
+  getJSOMFromAPI($sidebarSearchbar.elements[0].value);
   $sidebarSearchbar.reset();
 });
+
+function getJSOMFromAPI(endpoint) {
+  const xhr = new XMLHttpRequest();
+  // &order_by="popularity"&sort="asc"
+  // figure out the correct use of more of the parameters later
+  const targetUrl = encodeURIComponent('https://api.jikan.moe/v4/manga' + '?min_score=.4&q=' + endpoint);
+  xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log('xhr.status:', xhr.status);
+    // console.log('xhr.response:', xhr.response);
+  });
+  xhr.send();
+}
