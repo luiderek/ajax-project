bb7d4276168dc0270f31feb6097c58c95f1c548e
Derek Lui <luiderek@outlook.com>
2022-04-18 18:26:02 +0000

    adding searchbar to sidebar

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-const headbarMenu = document.querySelector('.headbar-menu-toggle');
-const sidebarMenu = document.querySelector('.sidebar-modal');
-const sidebarContainer = document.querySelector('.sidebar-container');
+const $headbarMenu = document.querySelector('.headbar-menu-toggle');
+const $sidebarMenu = document.querySelector('.sidebar-modal');
+const $sidebarContainer = document.querySelector('.sidebar-container');
 
-headbarMenu.addEventListener('click', function (event) {
+$headbarMenu.addEventListener('click', function (event) {
   sidebarVisibilityToggle();
 });
 
-sidebarMenu.addEventListener('click', function (event) {
+$sidebarMenu.addEventListener('click', function (event) {
   if (event.target.className.includes('blur')) {
     sidebarVisibilityToggle();
   }
 });
 
 function sidebarVisibilityToggle() {
-  sidebarMenu.classList.toggle('blur');
-  sidebarContainer.classList.toggle('hidden');
+  $sidebarMenu.classList.toggle('blur');
+  $sidebarContainer.classList.toggle('hidden');
 }
+
+const $sidebarSearchbar = document.querySelector('form#search');
+
+$sidebarSearchbar.addEventListener('submit', function (event) {
+  event.preventDefault();
+  // value of interest: $sidebarSearchbar.elements[0].value;
+  $sidebarSearchbar.reset();
+});
